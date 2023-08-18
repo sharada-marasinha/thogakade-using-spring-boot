c6ba91c6e3a74886674d252bc23f72b9963dbb67
sharada-marasinha <sharadamarasinha@gmail.com>
2023-08-18 17:00:54 +0530

    modify

--- src/main/java/edu/icet/dto/custom/Customer.java
+++ src/main/java/edu/icet/dto/custom/Customer.java
@@ -2,12 +2,14 @@ package edu.icet.dto.custom;
 
 import edu.icet.dto.SuperDTO;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Customer implements SuperDTO {
     private String id;
     private String name;
--- src/main/java/edu/icet/service/custom/impl/CustomerImpl.java
+++ src/main/java/edu/icet/service/custom/impl/CustomerImpl.java
@@ -3,12 +3,24 @@ package edu.icet.service.custom.impl;
 import edu.icet.dto.custom.Customer;
 import edu.icet.service.custom.CustomerService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerImpl implements CustomerService {
+    ArrayList<Customer> customerList;
+
+    public CustomerImpl() {
+        customerList=new ArrayList<>();
+        customerList.add(new Customer("C001","Saman","Galgamuwa",75000.00));
+        customerList.add(new Customer("C002","Anura","galnewa",77000.00));
+        customerList.add(new Customer("C003","Wimal","Gonagamuwa",72000.00));
+    }
+
     @Override
     public List<Customer> getAll() {
-        return null;
+
+        return this.customerList;
+
     }
 
     @Override
